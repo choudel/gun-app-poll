725d522ac995be6420fd38a0e7ff2e6701a15ff2
choudel <chouaibh@gmail.com>
2022-07-19 08:11:10 +0100

    todo app

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+// @ts-nocheck
+
+import { gun } from "./initGun"
+let input = ""
+const create = () => gun.get("todos").get(input).put({ title: input, done: false })
+let store = {}
+gun.get("todos").map().on(function(data, key) {
+    if (data) {
+        store[key] = data
+    } else {
+        delete store[key]
+        store = store
+    }
+})
+$: todos = Object.entries(store).sort((a, b) => a[1].done)
+</script>
+
+<input placeholder="Add todo" bind:value={input} />
+<button on:click={() => create() && (input = "")}>Add</button>
+
+{#each todos as [key, todo]}
+<div id={key}>
+    {todo.title} {todo.done ? "😺" : "😾"}
+</div>
+{/each}
